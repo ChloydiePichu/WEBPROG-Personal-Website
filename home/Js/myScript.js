a42ac8f19ce5aefc47c80b4c29370326af4f5485
Chloyd Lechico <137134954+ChloydiePichu@users.noreply.github.com>
2024-12-16 16:28:25 +0800

    Updated function

--- home/Js/myScript.js
+++ home/Js/myScript.js
@@ -1,11 +1,11 @@
 function myFunction() {
-    document.getElementById("demo").innerHTML = "What is my nickname";
+    document.getElementById("demo").innerHTML = "What is my nickname?";
   }
 
   function myFunction2() {
     const element = document.getElementsByTagName("c");
 
-    document.getElementById("demo").innerHTML = 'Its' + element[0].innerHTML;
+    document.getElementById("demo").innerHTML = 'Its ' + element[0].innerHTML;
 
 
   }
